b7b0823f1832eb10f12e6721e9ec37cfb7105806
jasonbdt <ja.bladt@gmail.com>
2025-09-29 00:48:40 +0200

    Add code to fulfill Task 2 (Send POST request)

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import requests
 URL_BASE = "https://learningserver.masterschool.com"
 
 def send_get(url: str, params: dict[str, str]) -> str:
-    res = requests.get(URL_BASE+url, params)
-    return res.text
+    return requests.get(URL_BASE+url, params).text
+
+
+def send_post(url: str, data: dict[str, str]) -> str:
+    return requests.post(URL_BASE+url, data).text
 
 
 def get_user_input(prompt: str) -> str:
@@ -12,12 +15,19 @@ def get_user_input(prompt: str) -> str:
 
 
 def main() -> None:
-    task1_params = {
-        "name": get_user_input("Enter name:"),
-        "color": get_user_input("Enter color:")
+    # task1_params = {
+    #     "name": get_user_input("Enter name:"),
+    #     "color": get_user_input("Enter color:")
+    # }
+    # task1 = send_get("/http-basics/get-me", task1_params)
+    # print(task1)
+
+    task2_data = {
+        "username": get_user_input("Enter Username:"),
+        "password": get_user_input("Enter Password:")
     }
-    task1 = send_get("/http-basics/get-me", task1_params)
-    print(task1)
+    task2 = send_post("/http-basics/post-me", task2_data)
+    print(task2)
 
 if __name__ == '__main__':
     main()
