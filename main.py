f307ab551cf092363a1281431c64da02fd6bc03c
jasonbdt <ja.bladt@gmail.com>
2025-09-29 12:33:00 +0200

    Refactor code to improve readability/maintainability

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+from typing import Any
 from api import *
 
 
 def get_user_input(prompt: str) -> str:
     return input(f"{prompt} ")
+
+
+def display_meal_info(meals: dict[str, Any]) -> None:
+    for meal_info in meals:
+        print(f" MEAL START ".center(40, "*"))
+        print(
+            f" {meal_info["strMeal"]} ".center(40, "*"),
+            f"\nCategory: {meal_info["strCategory"]}"
+            f"\nArea: {meal_info["strArea"]}"
+            f"\nInstructions:"
+            f"\n{meal_info["strInstructions"]}\n",
+        )
+        print(f" MEAL END ".center(40, "*"), "\n")
 
 
 def main() -> None:
@@ -15,18 +29,9 @@ def main() -> None:
     # print(task2(username, password))
 
     meal_name = get_user_input("Enter meal name:")
-    meals = get_meals(meal_name)
-    meals_len = len(meals)
-    print(f"We found {meals_len} meal(s):\n")
-
-    for meal_info in meals:
-        print(
-            f" {meal_info["strMeal"]} ".center(40, "*"),
-            f"\nCategory: {meal_info["strCategory"]}"
-            f"\nArea: {meal_info["strArea"]}"
-            f"\nInstructions:"
-            f"\n{meal_info["strInstructions"]}"
-        )
+    meals, meals_len = get_meals(meal_name)
+    print(f"We found {meals_len} meal(s):")
+    display_meal_info(meals)
 
 if __name__ == '__main__':
     main()
