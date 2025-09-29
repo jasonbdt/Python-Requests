524ce6efceb0647d7a9dd41d1770d357c3cc390c
jasonbdt <ja.bladt@gmail.com>
2025-09-29 13:14:45 +0200

    Add HolidayAPI requests

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 from api.themealdb import get_meals
 from api.masterschool import task1, task2
+from api.holidayapi import get_holidays, get_available_countries
 
-__all__ = ["get_meals", "task1", "task2"]
+__all__ = ["get_meals", "task1", "task2", "get_holidays", "get_available_countries"]
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Any
+from datetime import datetime
 from api import *
 
 
@@ -19,6 +20,28 @@ def display_meal_info(meals: dict[str, Any]) -> None:
         print(f" MEAL END ".center(40, "*"), "\n")
 
 
+def display_countries(countries: dict[str, Any]) -> None:
+    print("Available Countries:\n")
+    for country in countries:
+        print(f"({country['codes']['alpha-2']}) {country['name']}")
+    print()
+
+
+def display_holidays(holidays: dict[str, Any], year: int) -> None:
+    print(f"List of holidays in the last year ({year}):")
+    for holiday in holidays:
+        date_obj = datetime.fromisoformat(holiday['date'])
+        date_lst = date_obj.strftime("%A,%B,%d").split(',')
+        weekday, month, day = date_lst
+        suffix = (
+            "st" if day.endswith("1") else
+            "nd" if day.endswith("2") else
+            "rd" if day.endswith("3") else "th"
+        )
+        print(f"{holiday['name']} ({weekday}, {month} {day}{suffix})")
+    print()
+
+
 def main() -> None:
     # name = get_user_input("Enter name:")
     # color = get_user_input("Enter color:")
@@ -28,10 +51,18 @@ def main() -> None:
     # password = get_user_input("Enter Password:")
     # print(task2(username, password))
 
-    meal_name = get_user_input("Enter meal name:")
-    meals, meals_len = get_meals(meal_name)
-    print(f"We found {meals_len} meal(s):")
-    display_meal_info(meals)
+    # meal_name = get_user_input("Enter meal name:")
+    # meals, meals_len = get_meals(meal_name)
+    # print(f"We found {meals_len} meal(s):")
+    # display_meal_info(meals)
+
+    last_year = datetime.now().year - 1
+    countries, countries_len = get_available_countries()
+    display_countries(countries)
+
+    country_code = get_user_input("Enter Country (two-letter code):").upper()
+    holidays, holidays_len = get_holidays(country_code, last_year)
+    display_holidays(holidays, last_year)
 
 if __name__ == '__main__':
     main()
